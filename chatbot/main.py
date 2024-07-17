bcb677bc52c30a59213df1d71dea44792a7a4119
steven111212 <73684479+steven111212@users.noreply.github.com>
2024-07-17 16:19:16 +0800

    Add files via upload

---
+++ chatbot/main.py
@@ -0,0 +1,10 @@
+from chatbot.data_load import Dataload
+
+print("請輸入欲參考的文字或PDF檔案路徑或是參考網頁的網址 : ")
+input_information = input()
+test = Dataload(input_information)
+retriever = test.chunk_embedding()
+print("嗨! 我是你的助手Steven，你想問什麼問題呢 : ")
+while True:
+    input_text = input()
+    test.gogo(input_text)
